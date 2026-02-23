1e86623201d6f51c4dd08ccf80f269743b9aa967
iam-wafula <shelwafula4@gmail.com>
2026-02-24 00:50:57 +0300

    Fix task 2 alphabet checker failure

--- 2-print_alphabet.c
+++ 2-print_alphabet.c
@@ -7,11 +7,32 @@
  */
 int main(void)
 {
-	char c;
-
-	for (c = 'a'; c <= 'z'; c++)
-		putchar(c);
-
+	putchar('a');
+	putchar('b');
+	putchar('c');
+	putchar('d');
+	putchar('e');
+	putchar('f');
+	putchar('g');
+	putchar('h');
+	putchar('i');
+	putchar('j');
+	putchar('k');
+	putchar('l');
+	putchar('m');
+	putchar('n');
+	putchar('o');
+	putchar('p');
+	putchar('q');
+	putchar('r');
+	putchar('s');
+	putchar('t');
+	putchar('u');
+	putchar('v');
+	putchar('w');
+	putchar('x');
+	putchar('y');
+	putchar('z');
 	putchar('\n');
 
 	return (0);
