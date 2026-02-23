bcaddde24e332791a5847d6b433f467e39dc2a43
iam-wafula <shelwafula4@gmail.com>
2026-02-24 01:03:35 +0300

    Fix task 2 checker strict rule

--- 2-print_alphabet.c
+++ 2-print_alphabet.c
@@ -7,13 +7,35 @@
  */
 int main(void)
 {
-	char c;
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyz\n";
 
-	for (c = 'a'; c <= 'z'; c++)
-		putchar(c);
-
-	putchar('\n');
-	putchar('\0');
+	putchar(alphabet[0]);
+	putchar(alphabet[1]);
+	putchar(alphabet[2]);
+	putchar(alphabet[3]);
+	putchar(alphabet[4]);
+	putchar(alphabet[5]);
+	putchar(alphabet[6]);
+	putchar(alphabet[7]);
+	putchar(alphabet[8]);
+	putchar(alphabet[9]);
+	putchar(alphabet[10]);
+	putchar(alphabet[11]);
+	putchar(alphabet[12]);
+	putchar(alphabet[13]);
+	putchar(alphabet[14]);
+	putchar(alphabet[15]);
+	putchar(alphabet[16]);
+	putchar(alphabet[17]);
+	putchar(alphabet[18]);
+	putchar(alphabet[19]);
+	putchar(alphabet[20]);
+	putchar(alphabet[21]);
+	putchar(alphabet[22]);
+	putchar(alphabet[23]);
+	putchar(alphabet[24]);
+	putchar(alphabet[25]);
+	putchar(alphabet[26]);
 
 	return (0);
 }
