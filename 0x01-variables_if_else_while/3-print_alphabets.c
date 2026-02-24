c9bc0b6557b5bd785676828ef4f4e622662239e4
iam-wafula <shelwafula4@gmail.com>
2026-02-24 20:36:57 +0300

    Fix task 3 alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints lowercase and uppercase alphabet
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+
+	for (c = 'A'; c <= 'Z'; c++)
+		putchar(c);
+
+	putchar('\n');
+
+	return (0);
+}
