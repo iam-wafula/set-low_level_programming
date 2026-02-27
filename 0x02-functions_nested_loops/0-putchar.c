1c85e57619a8efcf3c0efc8cdceba23b0654561f
iam-wafula <shelwafula4@gmail.com>
2026-02-27 23:45:16 +0300

    Task 0 solution

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - prints _putchar
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char text[] = "_putchar";
+    int i;
+
+    for (i = 0; text[i] != '\0'; i++)
+    {
+        _putchar(text[i]);
+    }
+
+    _putchar('\n');
+
+    return (0);
+}
