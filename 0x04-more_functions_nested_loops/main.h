934be659c4e17a5726d55ec29ff1f26bd02c0396
iam-wafula <shelwafula4@gmail.com>
2026-03-06 23:17:05 +0300

    Completed 0x04-more_functions_nested_loops project

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+
+void print_triangle(int size);
+void print_square(int size);
+void print_diagonal(int n);
+void print_line(int n);
+void more_numbers(void);
+void print_most_numbers(void);
+
+#endif
