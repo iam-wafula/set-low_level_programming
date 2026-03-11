86da05ed1d16bd5d22f6da55f612b187cdde3ace
iam-wafula <shelwafula4@gmail.com>
2026-03-11 18:16:16 +0300

    Task 2: Fix compilation, Betty style, documented

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,18 +1,19 @@
 #include "main.h"
 
 /**
- * largest_number - returns the largest of 3 numbers
+ * largest_number - returns the largest of three integers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ *
+ * Return: largest integer
  */
-
 int largest_number(int a, int b, int c)
 {
-    int largest = a;
-
-    if (b > largest)
-        largest = b;
-
-    if (c > largest)
-        largest = c;
-
-    return (largest);
+	if (a >= b && a >= c)
+		return (a);
+	else if (b >= a && b >= c)
+		return (b);
+	else
+		return (c);
 }
