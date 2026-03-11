f0dba5dda70b90ae24661eb8e75b07e1282bda95
iam-wafula <shelwafula4@gmail.com>
2026-03-11 19:08:40 +0300

    Task 3: print_numbers function Betty-compliant

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * print_numbers - prints numbers from 0 to 9
+ * print_numbers - prints the numbers from 0 to 9
  *
  * Return: void
  */
 void print_numbers(void)
 {
-    char c;
+	char c;
 
-    for (c = '0'; c <= '9'; c++)
-        _putchar(c);
+	for (c = '0'; c <= '9'; c++)
+		_putchar(c);
 
-    _putchar('\n');
+	_putchar('\n');
 }
