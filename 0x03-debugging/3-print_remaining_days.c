378b9cf464888ce615c6829ce9d47d6089e73a70
iam-wafula <shelwafula4@gmail.com>
2026-03-11 18:37:07 +0300

    Task 3: Fixed leap year logic, Betty style, documented

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,20 +1,29 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * print_remaining_days - prints the day of the year and remaining days
+ *                        taking leap years into account
+ * @month: month number (1-12)
+ * @day: day of the year (converted from month/day)
+ * @year: full year
+ *
+ * Return: void
+ */
 void print_remaining_days(int month, int day, int year)
 {
-    int leap = 0;
+	if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+	{
+		/* Leap year adjustment: add 1 if date after Feb */
+		if (month > 2)
+			day++;
 
-    if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
-        leap = 1;
-
-    if (leap && month > 2)
-        day++;
-
-    printf("Day of the year: %d\n", day);
-
-    if (leap)
-        printf("Remaining days: %d\n", 366 - day);
-    else
-        printf("Remaining days: %d\n", 365 - day);
+		printf("Day of the year: %d\n", day);
+		printf("Remaining days: %d\n", 366 - day);
+	}
+	else
+	{
+		printf("Day of the year: %d\n", day);
+		printf("Remaining days: %d\n", 365 - day);
+	}
 }
