fc1529cc632594e35f8cd26e6d8c946af0ecbb22
iam-wafula <shelwafula4@gmail.com>
2026-03-26 23:11:42 +0300

    Task 0: _memset.c Betty coding and documentation style

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,12 +2,12 @@
 
 /**
  * _memset - fills memory with a constant byte
- * @s: pointer to the memory area
+ * @s: pointer to memory area
  * @b: byte to fill
  * @n: number of bytes to fill
  *
- * Description: This function fills the first n bytes of memory
- * pointed to by s with the constant byte b.
+ * Description: This function fills the first n bytes of the memory
+ * area pointed to by s with the constant byte b.
  * Return: pointer to the memory area s
  */
 char *_memset(char *s, char b, unsigned int n)
@@ -15,7 +15,9 @@ char *_memset(char *s, char b, unsigned int n)
     unsigned int i;
 
     for (i = 0; i < n; i++)
+    {
         s[i] = b;
+    }
 
     return (s);
 }
