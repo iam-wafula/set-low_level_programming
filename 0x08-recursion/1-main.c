a3d7d92b5c2df25526786a76c4848cf6875cff82
iam-wafula <shelwafula4@gmail.com>
2026-03-27 18:56:28 +0300

    Task 1: _print_rev_recursion function with main

---
+++ 0x08-recursion/1-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - check the _print_rev_recursion function
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    _print_rev_recursion("Colton Walker");
+    _putchar('\n');
+    return (0);
+}
